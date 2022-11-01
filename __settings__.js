05ba60db2212331f7bae3d3dd6ff511e486d5212
mishalibertee <40911200+mishalibertee@users.noreply.github.com>
2022-11-01 15:01:14 +0300

    init

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1577332.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 108749471, 108749479, 108749481, 108749495, 108749498 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/108749468/1/basis.wasm.js', 'wasmUrl' : 'files/assets/108749469/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/108749467/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/108749464/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/108749465/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/108749463/1/ammo.js', 'preload' : true},
+];
